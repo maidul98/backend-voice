f4a5c7eb100191ae1f3a6b691e37b515f031c0c5
Maidul Islam <maidul98@gmail.com>
2021-02-13 18:05:32 -0500

    added firebase config

--- app.js
+++ app.js
@@ -93,7 +93,19 @@ const serviceAccount = JSON.parse(
 );
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: "voice-app-303802",
+    private_key_id: "b9727cd1120a753cf645cd88e727bb2108c224ef",
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: "117153503833213314972",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-g9qw3%40voice-app-303802.iam.gserviceaccount.com",
+  }),
 });
 
 /**
